d1b1bffbabffda487e4a09e2d512deba0fd0af62
Johanny A. Mateo <24462857+johannymateo@users.noreply.github.com>
2019-07-03 02:36:18 -0400

    Commiting additional files

---
+++ Spotify iOS SDK Quick StartTests/Spotify_iOS_SDK_Quick_StartTests.m
@@ -0,0 +1,37 @@
+//
+//  Spotify_iOS_SDK_Quick_StartTests.m
+//  Spotify iOS SDK Quick StartTests
+//
+//  Created by Johanny Mateo on 7/3/19.
+//  Copyright © 2019 Johanny A. Mateo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Spotify_iOS_SDK_Quick_StartTests : XCTestCase
+
+@end
+
+@implementation Spotify_iOS_SDK_Quick_StartTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

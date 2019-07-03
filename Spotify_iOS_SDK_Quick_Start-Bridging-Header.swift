0a9849ae58d192c67dff62821f41d91cfe254ebd
Johanny A. Mateo <24462857+johannymateo@users.noreply.github.com>
2019-07-03 04:46:30 -0400

    - Changing from Objective C to Swift

--- Spotify_iOS_SDK_Quick_Start-Bridging-Header.swift
+++ Spotify_iOS_SDK_Quick_Start-Bridging-Header.swift
@@ -1,10 +1,9 @@
 //
-//  Spotify_iOS_SDK_Quick_Start-Bridging-Header.h
+//  Spotify_iOS_SDK_Quick_Start-Bridging-Header.swift
 //  Spotify iOS SDK Quick Start
 //
 //  Created by Johanny Mateo on 7/3/19.
 //  Copyright © 2019 Johanny A. Mateo. All rights reserved.
 //
 
-#import <SpotifyiOS/SpotifyiOS.h>
-/* Spotify_iOS_SDK_Quick_Start_Bridging_Header_h */
+import Foundation
